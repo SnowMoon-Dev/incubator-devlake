cc2dd1e447997756bb76b65b4c61f415a4ad90d6
Mr.An <42627702+SnowMoon-Dev@users.noreply.github.com>
2022-06-15 01:27:13 +0800

    update gitee code

--- plugins/gitee/models/pull_request.go
+++ plugins/gitee/models/pull_request.go
@@ -53,36 +53,6 @@ type GiteePullRequest struct {
 	AuthorId       int
 	common.NoPKModel
 }
-type GithubPullRequest struct {
-	GithubId        int    `gorm:"primaryKey"`
-	RepoId          int    `gorm:"index"`
-	Number          int    `gorm:"index"` // This number is used in GET requests to the API associated to reviewers / comments / etc.
-	State           string `gorm:"type:varchar(255)"`
-	Title           string `gorm:"type:varchar(255)"`
-	GithubCreatedAt time.Time
-	GithubUpdatedAt time.Time `gorm:"index"`
-	ClosedAt        *time.Time
-	// In order to get the following fields, we need to collect PRs individually from GitHub
-	Additions      int
-	Deletions      int
-	Comments       int
-	Commits        int
-	ReviewComments int
-	Merged         bool
-	MergedAt       *time.Time
-	Body           string
-	Type           string `gorm:"type:varchar(255)"`
-	Component      string `gorm:"type:varchar(255)"`
-	MergeCommitSha string `gorm:"type:varchar(40)"`
-	HeadRef        string `gorm:"type:varchar(255)"`
-	BaseRef        string `gorm:"type:varchar(255)"`
-	BaseCommitSha  string `gorm:"type:varchar(255)"`
-	HeadCommitSha  string `gorm:"type:varchar(255)"`
-	Url            string `gorm:"type:varchar(255)"`
-	AuthorName     string `gorm:"type:varchar(100)"`
-	AuthorId       int
-	common.NoPKModel
-}
 
 func (GiteePullRequest) TableName() string {
 	return "_tool_gitee_pull_requests"
